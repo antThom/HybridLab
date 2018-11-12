523a3d1de7701f3e930f74e87d3a5e3b5d02f783
Anthony <anthonyat@berkeley.edu>
2018-11-12 12:59:18 -0800

    Created Ros Kinetic Network

---
+++ Cassie/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cassie/Cassie/src
+# Build directory: /home/cassie/Cassie/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ Cassie/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cassie/Cassie/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/cassie/Cassie/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cassie/Cassie/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/cassie/Cassie/src:$ROS_PACKAGE_PATH"
